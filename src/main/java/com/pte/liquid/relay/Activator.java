579a0901b4b1ab3186c40b60db5ce01f9c1d845f
rjptegelaar <tegelaarpaul@gmail.com>
2016-09-05 13:52:15 +0200

    Init import

---
+++ src/main/java/com/pte/liquid/relay/Activator.java
@@ -0,0 +1,20 @@
+package com.pte.liquid.relay;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+	
+	
+    public void start(BundleContext context) throws Exception {    	
+    }
+
+    public void stop(BundleContext context) throws Exception {
+    	
+    }
+    
+  
+
+    	    
+
+}
